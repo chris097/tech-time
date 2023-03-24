f49675c95fed5f614823e1f8cbb6801dfcd55a33
chris097 <chrisfidel.international@gmail.com>
2023-03-24 15:25:52 +0100

    course page

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import Banner from '@/components/Banner'
 import Client from '@/components/Client'
 import Courses from '@/components/Courses'
+import About from '@/components/About'
 
 // const inter = Inter({ subsets: ['latin'] })
 
@@ -20,6 +21,7 @@ export default function Home() {
         <Banner />
         <Client />
         <Courses />
+        <About />
       </main>
     </>
   )
